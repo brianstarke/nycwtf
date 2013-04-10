7cd58e850faea9ae58c951243a22962303637137
Brian Starke <brian@isocket.com>
2013-04-09 22:36:12 -0400

    add new incidents to db

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var models = require('../models');
 /*
  * GET list of all incidents.
  */
-exports.index = function (req, res) {
-    models.incident.where().limit(25).run(function (err, docs) {
+exports.incidents = function (req, res) {
+    models.incident.find().limit(25).exec(function (err, docs) {
+        console.log("found " + docs.length);
         res.render('incidents', { title: 'NYC WTF', incidents: docs });
     });
 };
